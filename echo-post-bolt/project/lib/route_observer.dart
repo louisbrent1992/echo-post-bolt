9177acefe42d24a67d92c972c94eefde27d7eeac
louisbrent1992 <louisbrent1992@gmail.com>
2025-06-29 06:16:11 -0700

    deleted extra app folder

--- echo-post-bolt/project/lib/route_observer.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-/// Global [RouteObserver] that can be used across the app to watch route changes.
-///
-/// Register this in [MaterialApp.navigatorObservers] and subscribe via
-/// `routeObserver.subscribe(this, ModalRoute.of(context) as PageRoute)`.
-final RouteObserver<PageRoute<dynamic>> routeObserver =
-    RouteObserver<PageRoute<dynamic>>();
--- echo-post-bolt/project/lib/services/auth/platform_auth_services.dart
+++
@@ -1,7 +0,0 @@
-// Export all authentication services
-export '../account_auth_service.dart';
-export 'facebook_auth_service.dart';
-export 'instagram_auth_service.dart';
-export 'youtube_auth_service.dart';
-export 'twitter_auth_service.dart';
-export 'tiktok_auth_service.dart';
